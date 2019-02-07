2329ba4cf728be7224d2b93b4faf19f4acaf1f8c
Ashwanth A R <ashwanth1109@gmail.com>
2019-02-07 08:34:39 +0530

    Testing object mutation characteristics of functions

--- test.js
+++ test.js
@@ -1,19 +1,31 @@
-//------------------------------------------------------------------------
-// Section 1
-//------------------------------------------------------------------------
-const len = str => str.length;
-const name_len1 = ["Goku", "Vegeta", "Broly"].map(len);
-console.log(name_len1);
-//------------------------------------------------------------------------
-// Section 2
-//------------------------------------------------------------------------
-const name_len2 = ["Frieza", "Cell"].map(str => str.length);
-console.log(name_len2);
-//------------------------------------------------------------------------
-// Section 3
-//------------------------------------------------------------------------
-const reducer = (acc, curr) => acc + curr;
-const sum1 = [1, 2, 3, 4, 5].reduce(reducer, 0);
-console.log(sum1);
-const sum2 = [1, 2, 3, 4, 5].reduce((acc, curr) => acc + curr, 0);
-console.log(sum2);
+// //------------------------------------------------------------------------
+// // Section 1
+// //------------------------------------------------------------------------
+// const len = str => str.length;
+// const name_len1 = ["Goku", "Vegeta", "Broly"].map(len);
+// console.log(name_len1);
+// //------------------------------------------------------------------------
+// // Section 2
+// //------------------------------------------------------------------------
+// const name_len2 = ["Frieza", "Cell"].map(str => str.length);
+// console.log(name_len2);
+// //------------------------------------------------------------------------
+// // Section 3
+// //------------------------------------------------------------------------
+// const reducer = (acc, curr) => acc + curr;
+// const sum1 = [1, 2, 3, 4, 5].reduce(reducer, 0);
+// console.log(sum1);
+// const sum2 = [1, 2, 3, 4, 5].reduce((acc, curr) => acc + curr, 0);
+// console.log(sum2);
+
+const funA = num => {
+    num.ten = 10;
+    console.log(`Num inside fun ${JSON.stringify(num)}`);
+};
+
+let num = {
+    one: 1,
+    two: 2
+};
+funA(num);
+console.log(`Num outside fun ${JSON.stringify(num)}`);
