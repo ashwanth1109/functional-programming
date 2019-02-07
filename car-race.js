17d5e3ebe0bc1631e35db46cb06cddf94cd7ac48
Ashwanth A R <ashwanth1109@gmail.com>
2019-02-07 08:34:57 +0530

    Created functional version of car race program

--- car-race.js
+++ car-race.js
@@ -1,19 +1,56 @@
 //------------------------------------------------------------------------
 // IMPERATIVE PROGRAM
 //------------------------------------------------------------------------
-time = 5;
-carPositions = [1, 1, 1];
+// time = 5;
+// carPositions = [1, 1, 1];
+// while (time) {
+//     // decrease time
+//     time--;
+//     console.log("\n");
+//     for (let i = 0; i < carPositions.length; i++) {
+//         // move car
+//         if (Math.random() > 0.3) {
+//             carPositions[i] += 1;
+//         }
+//         // draw car
+//         let logStr = "";
+//         for (let j = 0; j < carPositions[i]; j++) logStr += "-";
+//         console.log(logStr);
+//     }
+// }
 
-while (time) {
-  // decrease time
-  time--;
-  for (let i = 0; i < carPositions.length; i++) {
-    console.log("\n");
-    // move car
-    if (Math.random() > 0.3) {
-      carPositions[i] += 1;
+// ------------------------------------------------------------
+// FUNCTIONAL PROGRAM
+// ------------------------------------------------------------
+// moveCars method
+const moveCars = carPositions => {
+    return carPositions.map(x => (Math.random() > 0.3 ? x + 1 : x));
+};
+// increaseTimeStep method
+const increaseTimeStep = state => {
+    return {
+        time: state.time - 1,
+        carPositions: moveCars(state.carPositions)
+    };
+};
+// outputCar method
+const outputCar = carPosition => {
+    return "-".repeat(carPosition);
+};
+// draw method
+const draw = carPositions => {
+    console.log(carPositions.map(outputCar).join("\n"));
+};
+// race method
+const race = state => {
+    console.log(`Time: ${state.time}`);
+    draw(state.carPositions);
+    if (state.time) {
+        race(increaseTimeStep(state));
     }
-    // draw car
-    console.log("-" * carPositions);
-  }
-}
+};
+// START OF THE PIPELINE - pass in the initial state
+race({
+    time: 5,
+    carPositions: [1, 1, 1]
+});
